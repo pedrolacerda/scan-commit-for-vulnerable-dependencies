8ca2604b48817edbdaffa95accffffe0c392e36b
Pedro Lacerda <pedrolacerda@github.com>
2020-03-16 18:26:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const graphql = require('@octokit/graphql')
 
-try {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-  // console.log(`Hello ${nameToGreet}!`);
-
-  // Example on how to set an output
-  //const time = (new Date()).toTimeString();
-  //core.setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-
-    const { vulnerability } = graphql(` query getVulnerability($ecosystem: String!, $package: String!){ 
-        securityVulnerabilities(ecosystem:$ecosystem:, first:10, package:$package) {
+async function getVulnerability(context){
+    let octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    let query = ` 
+    query { 
+        securityVulnerabilities(ecosystem: MAVEN, first:10, package:"com.hotels.styx:styx-api") {
             nodes {
                 firstPatchedVersion { identifier },
                 severity,
@@ -23,17 +13,19 @@ try {
                 vulnerableVersionRange
             }
         }
-    }`, {
-        ecosystem: "MAVEN",
-        package: "com.hotels.styx:styx-api",
-        headers: {
-            authorization: GITHUB_TOKEN
-        }
-    })
+    }`
 
+    return await octokit.graphql(query, {headers: {authorization: `token ${process.env.GITHUB_TOKEN}`}});
+}
+
+try {
+    let context = github.context
 
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+    let queryReturn = getVulnerability(context)
+    console.log(`The query return: ${queryReturn}`);
 
 } catch (error) {
   core.setFailed(error.message);
