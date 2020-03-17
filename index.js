6f751b4c07b8a95ff9711ec0e1034c732c500265
Pedro Lacerda <pedrolacerda@github.com>
2020-03-17 14:17:09 -0300

    New

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ try {
 
             // Create a list with the languages in the repo that can be scanned for vulnerabilities
             for (language in languagesEcosystems) {
-                console.log(languagesEcosystems.values)
+                console.log(values.language)
                 if(repoLanguagesSet.has(language))  languagesWithEcosystem.add(language)
             }
 
