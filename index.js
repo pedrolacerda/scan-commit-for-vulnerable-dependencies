ef909ec1d51661b988403c18161aa91c1e8a63f1
Pedro Lacerda <pedrolacerda@github.com>
2020-03-18 20:39:30 -0300

    asdf

--- index.js
+++ index.js
@@ -150,8 +150,8 @@ try {
                             for(i = 0; i < groupId["$$length"]; i++) {
                                 //console.log(groupId[i])
                                 //console.log(`Library [${i}]: ${groupId[i]}:${groupId[i].nextSibling} - version:${groupId[i].nextSibling.nextSibling}`)
-                                console.log(`Library [${i}]: ${(groupId[i]._ownerElement)}`)
-                                console.log(`Keys:${groupId[i].localName}`)
+                                console.log(`Library [${i}]: ${(Object.keys(groupId[i]._ownerElement))}\n`)
+                                console.log(`Keys:${groupId[i].localName}\n`)
                             }
                         })
                         .catch(error => core.setFailed(error.message));
