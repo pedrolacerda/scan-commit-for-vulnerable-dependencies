d6eac7ae14bac95a687d1e996e4c3b7ccbb7ebc0
Pedro Lacerda <pedrolacerda@github.com>
2020-03-18 19:01:01 -0300

    asdfsdf

--- index.js
+++ index.js
@@ -144,17 +144,17 @@ try {
                             let xmlDoc = parser.parseFromString(fileChanged)
 
                             // console.log(`xmlDoc.dependencies.nodeType: ${Object.keys(xmlDoc.getElementsByTagName('dependency'))}`)
-                            // console.log(`xmlDoc.dependencies.nodeType: ${xmlDoc.getElementsByTagName('dependency').length()}`)
-                            for(element in xmlDoc.getElementsByTagName('dependency')) {
-                                console.log(element)
-                            }
-                            
+                            console.log(`xmlDoc.dependencies.nodeType: ${xmlDoc.getElementsByTagName('dependency').length()}`)
+                            // for(element in xmlDoc.getElementsByTagName('dependency')) {
+                            //     console.log(element)
+                            // }
+
                             console.log(`=================================================================`)
                             // console.log(`xmlDoc.plugins:.nodeType:     ${Object.keys(xmlDoc.getElementsByTagName('plugin'))}`)
-                            // console.log(`xmlDoc.plugins:.nodeType:     ${xmlDoc.getElementsByTagName('plugin').length()}`)
-                            for(element in xmlDoc.getElementsByTagName('plugin')) {
-                                console.log(element)
-                            }
+                            console.log(`xmlDoc.plugins:.nodeType:     ${xmlDoc.getElementsByTagName('plugin').length()}`)
+                            // for(element in xmlDoc.getElementsByTagName('plugin')) {
+                            //     console.log(element)
+                            // }
                         })
                         .catch(error => core.setFailed(error.message));
                     }
