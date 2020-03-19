e207be2cf21a7926625c19d15e9b09fee2e25424
Pedro Lacerda <pedrolacerda@github.com>
2020-03-18 23:42:20 -0300

    Porra, mermão

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ async function getVulnerability(package, ecosystem) {
     
     let query = ` 
     query { 
-        securityVulnerabilities(ecosystem:MAVEN, first:100, package:${package}) {
+        securityVulnerabilities(ecosystem: MAVEN, first:100, package:${package}) {
             nodes {
                 firstPatchedVersion { identifier },
                 severity,
