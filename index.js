bc921a7c6a64756ffb6df3af9cfa526228636674
Pedro Lacerda <pedrolacerda@github.com>
2020-03-19 14:28:03 -0300

    Logger fixing

--- index.js
+++ index.js
@@ -178,10 +178,6 @@ try {
                                             if(vulnerability.firstPatchedVersion != null && typeof vulnerability.firstPatchedVersion !== 'undefined'){
                                                 // If the version of the package used is lower than the first patched version
                                                 // AND the first patched version of the package is bigger than minimun version registered so far
-                                                console.log(`Is [${version}] valid version?: ${semver.valid(semver.coerce(version.toString()))}`)
-                                                console.log(`Is [${vulnerability.firstPatchedVersion.identifier}] valid version?: ${semver.valid(semver.coerce(vulnerability.firstPatchedVersion.identifier.toString()))}`)
-                                                console.log(`Is [${minimumVersion}] valid version?: ${semver.valid(semver.coerce(minimumVersion.toString()))}`)
-
                                                 if((semver.compare(semver.valid(semver.coerce(version.toString())), semver.valid(semver.coerce(vulnerability.firstPatchedVersion.identifier.toString()))) == -1)
                                                 && (semver.compare(semver.valid(semver.coerce(vulnerability.firstPatchedVersion.identifier.toString())), semver.valid(semver.coerce(minimumVersion.toString()))) == 1)){
                                                     minimumVersion = vulnerability.firstPatchedVersion.identifier
